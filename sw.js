73da48208fea3fd114d4fa13d081ae98543abe48
SANJID ALAM <2403057@student.ruet.ac.bd>
2026-03-09 22:51:50 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
 
+self.addEventListener('install', (e) => {
+    console.log('Service Worker: Installed successfully!');
+});
+
+self.addEventListener('fetch', (e) => {
+    // This allows the app to work offline in the future if we want to upgrade it!
+});
